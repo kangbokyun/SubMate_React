7cde1cc9e1839f3aa7c1991ccb454bb1388ee1a9
강보균 <bk940204@naver.com>
2022-12-19 17:52:52 +0900

    홈 크롤링

--- src/Page/Main.js
+++ src/Page/Main.js
@@ -16,7 +16,9 @@ function Main() {
         setWindowHeight(window.innerHeight);
     };
 
-    const [rankDTO, setRankDTO] = useState([]);
+    const [ rankDTO, setRankDTO ] = useState([]);
+    const [ issueData, setIssueData ] = useState("");
+    const [ issueDTO, setIssueDTO ] = useState([]);
     
     useEffect(() => {
         const formData = new FormData();
@@ -25,7 +27,12 @@ function Main() {
         .then((res) => {
             console.log("/Home/Rank/Res : ", res);
             setRankDTO(res);
-        })
+        });
+        call("/Home/Issue", "POST", null)
+        .then((res) => {
+            console.log("/Home/Issue/Res : " + JSON.stringify(res));
+            setIssueDTO(JSON.stringify(res));
+        });
         resizeWindow();
         window.addEventListener("resize", resizeWindow);
         return () => window.removeEventListener("resize", resizeWindow);
@@ -42,27 +49,52 @@ function Main() {
             {window.innerWidth <= 767 ?
                 <div style = {{ marginBottom: "10vh", width: "100vw" }}>
                     <Carousel fade>
-                        { rankDTO.map((list) => 
+                        { rankDTO.map((list) =>
                             <Carousel.Item key = { list.mno }>
-                                <img alt = "Setting" src = { require('../MemberImg/' + list.profileImg) } style = {{ width: "100%", height: "30vh", maxHeight: "30vh", objectFit: "contain", backgroundColor: "gray" }} /> 
+                                <div style = {{ width: "100%"}}>
+                                    <img alt = "Setting" src = { require('../MemberImg/' + list.profileImg) } style = {{ width: "100%", height: "30vh", maxHeight: "30vh", objectFit: "contain", backgroundColor: "gray" }} /> 
+                                    <div className = "row" style = {{ backgroundColor: "rgba(0, 0, 0, 0.2)", color: "white", width: "100%", position: "absolute", bottom: "0", left: "0", marginLeft: "0.3vw" }}>
+                                        <div className = "col-6">
+                                            <div className = "row">
+                                                <div className = "col-2">
+                                                    <img alt = "Setting" src = { require('../IMG/BoardHeart_Red.png') } style = {{ width: "10vw", paddingTop: "2px" }} />
+                                                </div>
+                                                <div className = "col-4">
+                                                    <h5 style ={{ paddingTop: "1vh", paddingLeft: "2vw" }}>{ list.heartcount }</h5>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className = "col-6">
+                                            <div>
+                                                <h5 style = {{ paddingTop: "1vh", float: "right" }}>{ list.rankrankernickname }님</h5>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </Carousel.Item>
                         )}
                     </Carousel>
-                    <h1 style = {{ marginTop: "1.4vh" }}>공지사항</h1>
-                    <div id="showcase-dynamic" style = {{ height: "5vh", paddingTop: "0", backgroundColor: "transparent", color: "black", fontSize: "1.2rem" }}>
-                        <div><p>레이아웃을 잡는 중입니다.</p></div>
-                        <div><p>얼른 백엔드까지 연결하고 싶습니다.</p></div>
-                        <div><p>사실 제일 큰 문제는 JWT와 OAuth2입니다.</p></div>
+                    <h1 style = {{ marginTop: "1.4vh", marginLeft: "2vw" }}>공지사항</h1>
+                    <div id="showcase-dynamic" style = {{ height: "5vh", paddingTop: "0", backgroundColor: "transparent", color: "black", fontSize: "1.2rem", marginLeft: "2vw" }}>
+                        <div><p>공지사항1</p></div>
+                        <div><p>공지사항2</p></div>
+                        <div><p>공지사항3</p></div>
                     </div>
-                    <h1 style = {{ marginTop: "1.4vh" }}>서브뉴스</h1>
-                    <div id="showcase-dynamic" style = {{ height: "5vh", paddingTop: "0", backgroundColor: "transparent", color: "black", fontSize: "1.2rem" }}>
-                        <div><p>전장연시위</p></div>
-                        <div><p>지하철에서 사람이 쓰러졌어요!</p></div>
+                    <h1 style = {{ marginTop: "1.4vh", marginLeft: "2vw" }}>서브뉴스</h1>
+                    <div id="showcase-dynamic" style = {{ height: "5vh", paddingTop: "0", backgroundColor: "transparent", color: "black", fontSize: "1.2rem", marginLeft: "2vw" }}>
+                        {/* { issueDTO.map((list) => 
+                            <div key = {list.issueLink}>
+                                <p>{ list.issueTitle }</p>
+                            </div>
+                        )} */}
+                        <div>
+                            <p>{ issueDTO }</p>
+                        </div>
                     </div>
-                    <h1 style = {{ marginTop: "1.4vh" }}>오늘의 날씨</h1>
-                    <div id="showcase-dynamic" style = {{ height: "5vh", paddingTop: "0", backgroundColor: "transparent", color: "black", fontSize: "1.2rem" }}>
-                        <div><p>쌀쌀하긴 함</p></div>
-                        <div><p>맑음</p></div>
+                    <h1 style = {{ marginTop: "1.4vh", marginLeft: "2vw" }}>오늘의 날씨</h1>
+                    <div id="showcase-dynamic" style = {{ height: "5vh", paddingTop: "0", backgroundColor: "transparent", color: "black", fontSize: "1.2rem", marginLeft: "2vw" }}>
+                        <div><p>-10도</p></div>
+                        <div><p>추워요 롱패딩 입으세요.</p></div>
                     </div>
                 </div>
                 :

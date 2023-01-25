58630ea25dadf3c3759b4b5e27362884baf53668
kangbokyun <bk940204@naver.com>
2023-01-25 17:38:29 +0900

    채팅 엔터 전송

--- src/Page/Chat/InChat.js
+++ src/Page/Chat/InChat.js
@@ -193,6 +193,10 @@ function InChat() {
         };
 
         const sendPrivateValue = () => {
+            if(window.event.keyCode === 13) {
+                alert("EnterKeyUp");
+                return;
+            }
             // stompClient에 소켓이 존재한다면 /app/private-message로 값을 전달, 현재 room상태를 전달한다.
             // room은 개인아이디 상태 or CHATROOM(PublicChat)을 나타냄
             if(sotmpClient) {
@@ -211,6 +215,28 @@ function InChat() {
                 
                 console.log("sendPrivateValue_UserData : ", userData);
                 console.log("Check");
+            }
+        };
+
+        const sendEnterPrivateValue = () => {
+            if(window.event.keyCode === 13) {
+                if(sotmpClient) {
+                    let chatMessage = {
+                        senderName: userInfo.mnickname,
+                        message: userData.message,
+                        receiverName: chatDTO.state.roomname,
+                        senderno: chatDTO.state.senderno,
+                        mgender: userInfo.mnickname === chatDTO.state.sendername ? chatDTO.state.sgender : chatDTO.state.rgender,
+                        status: "MESSAGE"
+                    };
+    
+                    console.log("sendValue_Message : ", chatMessage);
+                    sotmpClient.send("/app/private-message", {}, JSON.stringify(chatMessage));
+                    setUserData({ ...userData, "message" : "" });
+                    
+                    console.log("sendPrivateValue_UserData : ", userData);
+                    console.log("Check");
+                }
             }
         };
 
@@ -342,7 +368,7 @@ function InChat() {
                         </ul>
                         {/* 메세지 보내는 곳 */}
                             <div className = "col-10 gx-0 col-md-9" style = {{  }}>
-                                <input className = "form-control" type = "text" placeholder = "메세지를 입력하세요." value = { userData.message } onChange = { handleMessage } style = {{ marginLeft: "4vw", width: "100%" }} />
+                                <input className = "form-control" onKeyUp = { sendEnterPrivateValue } type = "text" placeholder = "메세지를 입력하세요." value = { userData.message } onChange = { handleMessage } style = {{ marginLeft: "4vw", width: "100%" }} />
                             </div>
                             <div className = "col-2 gx-0 col-md-2" style = {{  }}>
                                 <button className = "btn btn-info" style = {{ width: "100%", paddingLeft: "1.7vw", color: "white", marginLeft: "5vw" }} type = "button" onClick = { sendPrivateValue }>Send</button>

9d4763d1118deeb19fcc9997371bad502e94866d
kangbokyun <bk940204@naver.com>
2023-02-06 17:52:56 +0900

    SubMate Update ver1.1.0

--- src/Page/Profile/MyProfile.js
+++ src/Page/Profile/MyProfile.js
@@ -1,22 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../Header';
 import Menu from '../Menu';
 
 function MyProfile() {
+    // 윈도우 크기 변경 감지되면 리렌더링
+    const [ windowWidth, setWindowWidth ] = useState(0);
+    const [ windowHeight, setWindowHeight ] = useState(0);
+    
+    let userInfo = JSON.parse(localStorage.getItem("UserInfo"));
+    
+    let resizeWindow = () => {
+        setWindowWidth(window.innerWidth);
+        setWindowHeight(window.innerHeight);
+    };
+
+    useEffect(() => {
+        resizeWindow();
+        window.addEventListener("resize", resizeWindow);
+        return () => window.removeEventListener("resize", resizeWindow);
+    }, []);
+    // /윈도우 크기 변경 감지되면 리렌더링
+    // 뒤로가기
+    const history = useNavigate();
+    const GoBack = () => {
+        return history(-1) // 한 페이지 뒤로
+    };
+
     return(
         <div>
             <Header />
             { window.innerWidth <= 767 ? 
                 <div><h1 style = {{ marginLeft: "3vw", marginTop: "8vh" }}>MyProfile
-                <span style = {{ float: "right", marginRight: "2vw" }}>
-                        <Link to = "/BoardWrite">
-                            <button type = "button" className = "btn btn-success">정보수정</button>
-                        </Link>
+                    <span style = {{ float: "right", marginRight: "2vw" }}>
+                        <button type = "button" className = "btn btn-success">정보수정</button>
                     </span>
                 </h1></div> :
                 <h1 style = {{ marginLeft: "6vw", marginTop: "8vh" }}>MyProfile</h1> 
             }
+            <div className = { window.innerWidth <= 767 ? "container" : "" }>
+                <div className = "row" style = {{ width: "100%", marginTop: "3vh" }}>
+                
+                </div>
+            </div>
             <Menu />
         </div>
     );

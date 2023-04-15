3686f2675f918eb19601f9f8c8fc1a84c8731aa0
kangbokyun <91529033+kangbokyun@users.noreply.github.com>
2023-04-15 21:23:20 +0900

    Update MyProfile.js

--- src/Page/Profile/MyProfile.js
+++ src/Page/Profile/MyProfile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect } from 'react'; 
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ChangeInfo, ChangeInfoImg, call } from '../../Service/APIService';
